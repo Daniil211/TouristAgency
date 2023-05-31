cb4e964e31aa8247b09b932c32cbd331f0c10458
Daniil211 <id20214@gmail.com>
2023-05-31 19:14:31 +0300

    Перенес мобильное приложение в общее решение

---
+++ Application.Mobile/Application.Mobile/AllTours.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Application.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllTours : ContentPage
+    {
+        public AllTours()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonMain_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ Application.Mobile/Application.Mobile/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Application.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonPod_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FoundedTours());
+        }
+        private async void ButtonAll_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AllTours());
+        }
+
+        private async void ButtonOrd_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new OrderList());
+        }
+    }
+}
