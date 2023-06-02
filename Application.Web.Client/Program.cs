4f39224edfc9d44c704f9c1850e0290fe38e7d89
Daniil211 <id20214@gmail.com>
2023-06-02 11:47:44 +0300

    Настроил Program.cs

--- Application.Web.Client/Program.cs
+++ Application.Web.Client/Program.cs
@@ -1,4 +1,7 @@
+using Application.Authorize.Utils;
 using Application.Web.Client;
+using Application.Web.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,27 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore(options => options.AddAppPolicies());
 
-await builder.Build().RunAsync();
+builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddScoped<IClientAuthenticationService, ClientAuthenticationService>();
+builder.Services.AddScoped<HttpService>();
+
+builder.Services.AddScoped(x =>
+{
+
+    Uri apiUrl;
+    if (builder.HostEnvironment.IsDevelopment())
+    {
+        apiUrl = new Uri(builder.Configuration["localAPIUrl"]);
+    }
+    else
+    {
+        apiUrl = new Uri(builder.Configuration["siteAPIUrl"]);
+    }
+
+    return new HttpClient() { BaseAddress = apiUrl };
+});
+
+//await builder.Build().RunAsync();
