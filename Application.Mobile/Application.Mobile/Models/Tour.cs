27b2d95a1fb45111ddb2fb06e4fe5ba21f36cb88
Daniil211 <id20214@gmail.com>
2023-06-05 20:40:49 +0300

    Service, Реализованные методы для web api сервера

--- Application.Mobile/Application.Mobile/Models/Tour.cs
+++ Application.Mobile/Application.Mobile/Models/Tour.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.Json.Serialization;
 
-namespace XamWebApiClient.Models
+namespace Application.Mobile.Models
 {
     public class Tour
     {
---
+++ Application.Mobile/Application.Mobile/Services/Services.Tour/InMemoryTourService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XamWebApiClient.Models;
+
+namespace Application.Mobile.Services.Services.Tour
+{
+    public class InMemoryTourService : ITourService
+    {
+        private readonly List<Book> _books = new List<Book>();
+        public InMemoryTourService()
+        {
+            _books.Add(new Book { TourName = "Clean code", Duration = "Robert C Martin", Description = "A book about good code", Image = "test", InSale = true, VideoTour = "test", Price = 1000 });
+            _books.Add(new Book { TourName = "Clean code", Duration = "Robert C Martin", Description = "A book about good code", Image = "test", InSale = true, VideoTour = "test", Price = 1000 });
+            _books.Add(new Book { TourName = "Clean code", Duration = "Robert C Martin", Description = "A book about good code", Image = "test", InSale = true, VideoTour = "test", Price = 1000 });
+        }
+
+        public Task AddBook(Book book)
+        {
+            book.TourId = ++_books.Last().TourId;
+            _books.Add(book);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteBook(Book book)
+        {
+            _books.Remove(book);
+            return Task.CompletedTask;
+        }
+
+        public Task<Book> GetBook(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.TourId == id);
+            return Task.FromResult(book);
+        }
+
+        public Task<IEnumerable<Book>> GetBooks()
+        {
+            return Task.FromResult(_books.AsEnumerable());
+        }
+
+        public Task SaveBook(Book book)
+        {
+            _books[_books.FindIndex(b => b.TourId == book.TourId)] = book;
+            return Task.CompletedTask;
+        }
+    }
+}
