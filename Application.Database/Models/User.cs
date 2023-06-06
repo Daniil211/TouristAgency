2ca5b30127fd2887a74c6dbc7f9dc0774c73b55a
Daniil211 <id20214@gmail.com>
2023-06-06 06:51:54 +0300

    убрад required поля, post работает

--- Application.Database/Models/User.cs
+++ Application.Database/Models/User.cs
@@ -20,14 +20,12 @@ namespace Application.Database.Models
         /// Gets or sets the username.
         /// </summary>
         /// <value>The username.</value>
-        [Required]
         public string Username { get; set; }
 
         /// <summary>
         /// Gets or sets the password.
         /// </summary>
         /// <value>The password.</value>
-        [Required]
         public string Password { get; set; }
 
         /// <summary>
