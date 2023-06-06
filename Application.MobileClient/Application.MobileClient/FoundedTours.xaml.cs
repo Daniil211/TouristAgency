3448102646a8cb947900f6e855ebf4ffc59073b5
Daniil211 <id20214@gmail.com>
2023-06-06 16:01:54 +0300

    Перенесен макет проекта мобильного на основу

---
+++ Application.MobileClient/Application.MobileClient/FoundedTours.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Application.MobileClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FoundedTours : ContentPage
+	{
+		public FoundedTours ()
+		{
+			InitializeComponent ();
+		}
+        private async void ButtonMain_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
--- Application.MobileClient/Application.MobileClient/MainPage.xaml.cs
+++ Application.MobileClient/Application.MobileClient/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Application.MobileClient
 {
@@ -7,6 +13,20 @@ namespace Application.MobileClient
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void ButtonPod_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FoundedTours());
+        }
+        private async void ButtonAll_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AllTours());
+        }
+
+        private async void ButtonOrd_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new OrderList());
         }
     }
 }
