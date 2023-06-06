cf256ab85b5835cb802514bc0abb40cbd705912d
Daniil211 <id20214@gmail.com>
2023-06-06 14:27:02 +0300

    fix conflict

--- Application.Desktop/OrderWindow.xaml.cs
+++ Application.Desktop/OrderWindow.xaml.cs
@@ -39,8 +39,16 @@ namespace Application.Desktop
             Order ord = new Order();
             var curtourdb = db.Tours.Where(x => x.TourName == Tour_cb.SelectedItem.ToString()).FirstOrDefault().TourId;
             ord.TourId = curtourdb;
-    
-
+            //тут добавить потом фиксацию айдишника при авторизации
+            ord.UserId = Id;
+            db.Orders.Add(ord);
+            db.SaveChanges();
+            MessageBox.Show("Заявка оставлена");
+            ord.UserId = 1;
+            ord.TourOperatorId = 1;
+            db.Orders.Add(ord);
+            db.SaveChanges();
+            MessageBox.Show("Заявка оставлена");
         }
     }
 }
