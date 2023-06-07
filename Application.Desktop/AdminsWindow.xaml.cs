ca64d16cec4f0a06c6d54c47d5595e39b147a1f0
Элина Хамитова <khamitovaelina09@gmail.com>
2023-06-07 23:43:02 +0300

    добавила обработчик исключений на пробелы, на некорректный возраст в добавлении туроператора

--- Application.Desktop/AdminsWindow.xaml.cs
+++ Application.Desktop/AdminsWindow.xaml.cs
@@ -28,34 +28,9 @@ namespace Application.Desktop
         public AdminsWindow()
         {
             InitializeComponent();
-            db = new TourAgencyContext();
-            var tbOrders = from men in db.TourOperators
-                           select new
-                           {
-                               IdMenedger = men.OperatorId,
-                               FIOMenedger = men.Fio,
-                               AgeMenedger = men.Age,
-                           };
-            DataGridMen.ItemsSource = tbOrders.ToList();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Image Files (*.jpg;*.png;*.gif;*.bmp)|*.jpg;*.png;*.gif;*.bmp|All Files (*.*)|*.*";
-            if (openFileDialog.ShowDialog() == true)
+            try
             {
-                string imagePath = openFileDialog.FileName;
-                base64String = WorkWithImagesMet.ConvertImgToBase64(imagePath);
-            }
-        }
-
-        private void save_btn_Click(object sender, RoutedEventArgs e)
-        {
-            //применить метод добавления
-            if (tb1.Text != "" && tb2.Text != "" && tb3.Text != "")
-            {
-                AddMenedgerMet.AddMenedgerMethod(tb1.Text, Convert.ToInt32(tb2.Text), tb3.Text, base64String);
+                db = new TourAgencyContext();
                 var tbOrders = from men in db.TourOperators
                                select new
                                {
@@ -65,9 +40,71 @@ namespace Application.Desktop
                                };
                 DataGridMen.ItemsSource = tbOrders.ToList();
             }
-            else
+            catch { MessageBox.Show("Ошибка сервера"); }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                MessageBox.Show("Заполните все поля");
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files (*.jpg;*.png;*.gif;*.bmp)|*.jpg;*.png;*.gif;*.bmp|All Files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string imagePath = openFileDialog.FileName;
+                    base64String = WorkWithImagesMet.ConvertImgToBase64(imagePath);
+                }
+            }
+           catch { MessageBox.Show("Ошибка сервера"); }
+        }
+
+        private void save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // метод добавления
+            try
+            {
+                if (tb1.Text != "" && tb2.Text != "" && tb3.Text != "")
+                {
+                    for (int i = 0; i < tb1.Text.Length; i++)
+                    {
+                        char ch = Convert.ToChar(tb1.Text.Substring(i, 1));
+                        if (ch == ' ')
+                        { MessageBox.Show("Введен пробел в поле ФИО. Невозможно добавить пользователя"); return; }
+                    } 
+                    for (int i = 0; i < tb2.Text.Length; i++)
+                    {
+                        char ch = Convert.ToChar(tb2.Text.Substring(i, 1));
+                        if (ch == ' ')
+                        { MessageBox.Show("Введен пробел в поле возраст. Невозможно добавить пользователя"); return; }
+                    }
+                    for (int i = 0; i < tb3.Text.Length; i++)
+                    {
+                        char ch = Convert.ToChar(tb3.Text.Substring(i, 1));
+                        if (ch == ' ')
+                        { MessageBox.Show("Введен пробел в поле резюме. Невозможно зарегистрироваться"); return; }
+                    }
+                    if (Convert.ToInt32(tb2.Text) <= 18 && Convert.ToInt32(tb2.Text) >= 82)
+                    {
+                        MessageBox.Show("Введен некорректный возраст. Невозможно добавить туроператора"); return;
+                    }
+                    AddMenedgerMet.AddMenedgerMethod(tb1.Text, Convert.ToInt32(tb2.Text), tb3.Text, base64String);
+                    var tbOrders = from men in db.TourOperators
+                                   select new
+                                   {
+                                       IdMenedger = men.OperatorId,
+                                       FIOMenedger = men.Fio,
+                                       AgeMenedger = men.Age,
+                                   };
+                    DataGridMen.ItemsSource = tbOrders.ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Заполните все поля");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка сервера");
             }
         }
     }
