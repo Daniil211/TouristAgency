6833405a88c3745a84df9a34388f1e0547c9db4e
Элина Хамитова <khamitovaelina09@gmail.com>
2023-06-07 23:17:24 +0300

    обработчик исключений на метод регистрации

--- Application.Desktop/Methods/RegMet.cs
+++ Application.Desktop/Methods/RegMet.cs
@@ -13,31 +13,36 @@ namespace Application.Desktop.Methods
     {
         public static User RegMethod(string login, string password, string fio, DateTime date, string phone)
         {
-            TourAgencyContext db = new();
+            try
+            {
+                TourAgencyContext db = new();
 
-            var user = db.Users.FirstOrDefault(x => x.Username == login);
-            if (user == null)
-            {
-                User u = new();
-                u.Username = login;
-                u.Role = "User";
-                u.Password = password;
-                u.IsPremiumMember = false;
-                u.DateCreated = DateTime.Now;
-                u.Fio = fio;
-                u.DateOfBirth = date;
-                u.Phone = phone;
-                u.Age = ((int)(DateTime.Now - date).TotalDays)/365;
-                db.Users.Add(u);
-                db.SaveChanges();
-                MessageBox.Show("Регистрация прошла успешно");
-                return u;
+                var user = db.Users.FirstOrDefault(x => x.Username == login);
+                if (user == null)
+                {
+                    User u = new();
+                    u.Username = login;
+                    u.Role = "User";
+                    u.Password = password;
+                    u.IsPremiumMember = false;
+                    u.DateCreated = DateTime.Now;
+                    u.Fio = fio;
+                    u.DateOfBirth = date;
+                    u.Phone = phone;
+                    u.Age = ((int)(DateTime.Now - date).TotalDays) / 365;
+                    db.Users.Add(u);
+                    db.SaveChanges();
+                    MessageBox.Show("Регистрация прошла успешно");
+                    return u;
+                }
+                else
+                {
+                    MessageBox.Show("Такой пользователь уже существует. Придумайте новый логин"); return null;
+                }
             }
-            else
-            {
-                MessageBox.Show("Такой пользователь уже существует. Придумайте новый логин");
-            }
-            return null;
+            catch {MessageBox.Show("Ошибка сервера"); return null; }
+          
+        
         }
     }
 }
